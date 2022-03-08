38989273f1f96a03f4a9cc553321ad041beab43e
boyoungs <101075082+boyoungs@users.noreply.github.com>
2022-03-08 11:34:16 +0900

    Add files via upload

---
+++ weekly/src/weekly/datatype.java
@@ -0,0 +1,12 @@
+package weekly;
+
+public class datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("202022306 gwon bo young");
+		System.out.println(System.getProperty("user.dir"));
+		
+	}
+
+}
